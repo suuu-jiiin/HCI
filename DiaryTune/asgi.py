89377e0c909cce9b524183255defecb4fce4fc6a
suuu-jiiin <102743714+suuu-jiiin@users.noreply.github.com>
2024-11-12 09:43:55 +0900

    REFACTOR: Rename project from HCI to DiaryTune

--- DiaryTune/asgi.py
+++ DiaryTune/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HCI project.
+ASGI config for DiaryTune project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HCI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DiaryTune.settings")
 
 application = get_asgi_application()
--- DiaryTune/wsgi.py
+++ DiaryTune/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for HCI project.
+WSGI config for DiaryTune project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HCI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DiaryTune.settings")
 
 application = get_wsgi_application()
